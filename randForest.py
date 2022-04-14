b7ab5fc8408b8593b29a3628b29032641fd95455
lukevrobbins <lukevrobbins@gmail.com>
2022-04-13 19:52:39 -0500

    Pulled RF apart. Ready to iterate

--- randForest.py
+++ randForest.py
@@ -1,30 +1,54 @@
-# evaluate a logistic regression model using k-fold cross-validation
+import pandas as pd
+import matplotlib.pyplot as plt
+
+#Tree Feature Selection 
+from asyncio.windows_events import NULL
+from sklearn.ensemble import RandomForestClassifier 
+
+# Import the RFE from sklearn library
+from sklearn.feature_selection import RFE
+import seaborn as sns 
+from sklearn.ensemble import RandomForestClassifier 
+from sklearn import metrics
+
+# evaluate a model using k-fold cross-validation
 from numpy import mean
-from numpy import std
 from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 
-# random forest
-from sklearn.ensemble import RandomForestClassifier
+def rand_forest(X_scaled, y_train, X, max_depth = None):
+	# getting radom forest without feature selection 
+    # Setting RF Model
+    # clf = RandomForestClassifier(n_estimators = 100, random_state=0)
+    # clf.fit(X_scaled, y_train)
 
-def rf_pred(X_scaled, y_train, max_depth):
+    # test_score = clf.score(X_scaled, y_train)
 
-	# Setting RF Model
-	clf = RandomForestClassifier(max_depth = max_depth, random_state=0)
-	clf.fit(X_scaled, y_train)
+    # Cross Validating
+    # cv = KFold(n_splits = 10, random_state = 1, shuffle = True)
 
-	test_score = clf.score(X_scaled, y_train)
+    # create model
+    model = RandomForestClassifier(n_estimators = 100, random_state=0, max_depth = max_depth)
+    model.fit(X_scaled, y_train)
+    test_score = model.score(X_scaled, y_train)
 
-	# Cross Validating
-	cv = KFold(n_splits = 10, random_state = 1, shuffle = True)
+    # evaluate model
+    scores = cross_val_score(model, X_scaled, y_train, scoring='accuracy', cv=10, n_jobs=-1)
 
-	# create model
-	model = RandomForestClassifier(max_depth = max_depth, random_state=0)
+    # reporting performance on full tree
+    print('Accuracy of full random forest on test data: %.3f || CV Accuracy: %.3f' % (test_score , mean(scores)))
 
-	# evaluate model
-	scores = cross_val_score(model, X_scaled, y_train, scoring='accuracy', cv=cv, n_jobs=-1)
 
-	# report performance
-	print('Accuracy on test data: %.3f || CV Accuracy: %.3f' % (test_score , mean(scores)))
+    # get the importance of the resulting features.
+    importances = model.feature_importances_
+    # create a data frame for visualization.
+    final_df = pd.DataFrame({"Features": X.columns, "Importances":importances})
+    final_df.set_index('Importances')
 
-	return clf
+    # sort in ascending order to better visualization.
+    final_df = final_df.sort_values('Importances')
+
+    # plot the feature importances in bars. 
+    plt.figure(figsize=(100,20))
+    plt.xticks(rotation=45)
+    sns.barplot(x="Features",y= "Importances", data=final_df)
--- treeFeatureSelect.py
+++ treeFeatureSelect.py
@@ -27,61 +27,22 @@ from sklearn.model_selection import KFold
 from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import LogisticRegression
 
-def treeFeatureSelection(X_scaled, y_train, FeatureAmount,X): 
-    #getting radom forest without feature selection 
-    # Setting RF Model
-    clf = RandomForestClassifier(n_estimators = 100, random_state=0)
-    clf.fit(X_scaled, y_train)
-
-    test_score = clf.score(X_scaled, y_train)
-
-    # Cross Validating
-    cv = KFold(n_splits = 10, random_state = 1, shuffle = True)
-
-    # create model
-    model = RandomForestClassifier(n_estimators = 100, random_state=0)
-
-    # evaluate model
-    scores = cross_val_score(model, X_scaled, y_train, scoring='accuracy', cv=cv, n_jobs=-1)
-
-    # reporting performance on full tree
-    print('Accuracy of full random forest on test data: %.3f || CV Accuracy: %.3f' % (test_score , mean(scores)))
-
-
-    # get the importance of the resulting features.
-    importances = clf.feature_importances_
-    # create a data frame for visualization.
-    final_df = pd.DataFrame({"Features": X.columns, "Importances":importances})
-    final_df.set_index('Importances')
-
-    # sort in ascending order to better visualization.
-    final_df = final_df.sort_values('Importances')
-
-    # plot the feature importances in bars. 
-    plt.figure(figsize=(100,20))
-    plt.xticks(rotation=45)
-    sns.barplot(x="Features",y= "Importances", data=final_df)
-
-
-    model_tree = RandomForestClassifier(n_estimators=100,random_state=42)
+def treeFeatureSelection(X_scaled, y_train, FeatureAmount, max_depth = None): 
+    model = RandomForestClassifier(n_estimators=100, random_state=0, max_depth = max_depth)
 
     # use RFE to eleminate the less importance features
-    sel_rfe_tree = RFE(estimator=model_tree, n_features_to_select=FeatureAmount, step=1)
-    X_train_rfe_tree = sel_rfe_tree.fit_transform(X_scaled, y_train)
-
+    sel_rfe_tree = RFE(estimator = model, n_features_to_select = FeatureAmount, step = 1)
+    sel_rfe_tree.fit_transform(X_scaled, y_train)
 
     #Reduce X to the selected features and then predict using the predict
     test_score = sel_rfe_tree.score(X_scaled, y_train)
 
-    # Cross Validating
-    cv = KFold(n_splits = 10, random_state = 1, shuffle = True)
-
     # create model
     model = RandomForestClassifier(n_estimators = 100, random_state=42)
-    rfe_tree = RFE(estimator=model, n_features_to_select=FeatureAmount, step=1)
+    rfe_tree = RFE(estimator = model, n_features_to_select = FeatureAmount, step = 1)
 
     # evaluate model
-    scores = cross_val_score(rfe_tree, X_scaled, y_train, scoring='accuracy', cv=cv, n_jobs=-1)
+    scores = cross_val_score(rfe_tree, X_scaled, y_train, scoring = 'accuracy', cv = 10, n_jobs=-1)
 
     # report performance
     print('Accuracy of feature selected random forest on test data: %.3f || CV Accuracy: %.3f' % (test_score , mean(scores)))
